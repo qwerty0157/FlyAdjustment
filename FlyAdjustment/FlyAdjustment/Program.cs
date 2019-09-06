1393bbea96af4fe1e77deebcd4933309a424a511
qwerty0157 <qwerty0157@gmail.com>
2019-09-07 00:59:01 +0900

    initial commit

---
+++ FlyAdjustment/FlyAdjustment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlyAdjustment
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
